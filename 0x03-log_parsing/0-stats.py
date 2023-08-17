ac5c511d98c17fcc8a2ae74840d8d0b2756789d4
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-08-17 10:52:27 +0000

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,44 +4,42 @@
 
 import sys
 
-def print_statistics(statistics, total_size):
-    """Prints information"""
-    print("File size: {:d}".format(total_size))
-    for status_code, count in sorted(statistics.items()):
-        if count != 0:
-            print("{}: {:d}".format(status_code, count))
 
-statistics = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-              "404": 0, "405": 0, "500": 0}
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-total_count = 0
-total_size = 0
+
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        if total_count != 0 and total_count % 10 == 0:
-            print_statistics(statistics, total_size)
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-        tokens = line.split()
-        total_count += 1
+        stlist = line.split()
+        count += 1
 
         try:
-            size = int(tokens[-1])
-            total_size += size
-        except IndexError:
-            pass
-        except ValueError:
+            size += int(stlist[-1])
+        except:
             pass
 
         try:
-            status_code = tokens[-2]
-            if status_code in statistics:
-                statistics[status_code] += 1
-        except IndexError:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
             pass
+    printsts(sts, size)
 
-    print_statistics(statistics, total_size)
 
 except KeyboardInterrupt:
-    print_statistics(statistics, total_size)
+    printsts(sts, size)
     raise
